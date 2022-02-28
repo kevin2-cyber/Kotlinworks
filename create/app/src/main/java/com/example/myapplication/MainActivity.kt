f8178cfaee2d08ade69c42c5f7c7eccfd81803c0
kevin2-cyber <keduful@st.ug.edu.gh>
2022-02-28 00:28:33 +0000

    People's counter app with kotlin

--- create/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ create/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.myButton)
+        val tvMyTextView = findViewById<TextView>(R.id.textView)
+        var timesClicked = 0
+        btnClickMe.setOnClickListener {
+            timesClicked += 1
+            tvMyTextView.text = timesClicked.toString()
+            Toast.makeText(this, "You clicked $tvMyTextView times", Toast.LENGTH_LONG).show()
+        }
     }
 }
