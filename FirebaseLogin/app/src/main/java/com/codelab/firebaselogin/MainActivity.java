539224fd0def9d3bf4b7a95c20ba749f6b958322
kevin2-cyber <keduful@st.ug.edu.gh>
2022-05-19 17:49:05 +0000

    added welcome activity

--- FirebaseLogin/app/src/main/java/com/codelab/firebaselogin/MainActivity.java
+++ FirebaseLogin/app/src/main/java/com/codelab/firebaselogin/MainActivity.java
@@ -2,18 +2,27 @@ package com.codelab.firebaselogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.codelab.firebaselogin.databinding.ActivityMainBinding;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static ActivityMainBinding binding;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        binding.btnTv.setOnClickListener(view -> {
+            startActivity(new Intent(this, WelcomeActivity.class));
+        });
     }
 }
