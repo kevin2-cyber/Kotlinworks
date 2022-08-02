d83899a696b9b176655bbd87f4a467846082eb44
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-02 14:06:42 +0000

    new

--- Tial/app/src/main/java/kevin/codelab/tial/MainActivity.java
+++ Tial/app/src/main/java/kevin/codelab/tial/MainActivity.java
@@ -1,15 +1,33 @@
 package kevin.codelab.tial;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+
+import kevin.codelab.tial.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mBinding;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(mBinding.getRoot());
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        String appName = "Kelvin";
+
+        mBinding.textView.setText("Hello " + appName);
+
     }
 }
