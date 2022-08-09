f3c2c7ec9d7acf655d67c44ff4b0210df330e106
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-09 13:55:51 +0000

    tried something

--- Tial/app/src/main/java/kevin/codelab/tial/MainActivity.java
+++ Tial/app/src/main/java/kevin/codelab/tial/MainActivity.java
@@ -1,33 +1,23 @@
 package kevin.codelab.tial;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Objects;
 
 import kevin.codelab.tial.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding mBinding;
+    ActivityMainBinding binding;
+    String email = "kimikevin@zoho.com";
+    String password = "12345savage";
 
-    @SuppressLint("SetTextI18n")
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+    protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(mBinding.getRoot());
-
-        Objects.requireNonNull(getSupportActionBar()).hide();
-
-        String appName = "Kelvin";
-
-        mBinding.textView.setText("Hello " + appName);
+        setContentView(binding.getRoot());
 
     }
 }
