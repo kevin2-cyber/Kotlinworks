0d123acaea0151b3b4955392474025cd71b61b47
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-18 00:59:31 +0000

    added the LiveData class

--- AndroidRoom/app/src/main/java/android/room/view/WordDao.java
+++ AndroidRoom/app/src/main/java/android/room/view/WordDao.java
@@ -1,5 +1,6 @@
 package android.room.view;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
@@ -19,6 +20,6 @@ public interface WordDao {
     void deleteAll();
 
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    List<Word> getAlphabetizedWords();
+    LiveData<List<Word>> getAlphabetizedWords();
 
 }
