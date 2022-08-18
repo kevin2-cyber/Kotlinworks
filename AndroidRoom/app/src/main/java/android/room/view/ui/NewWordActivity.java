068be0b3bea7bd2ce9bae8b1a675b992f6722bf5
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-18 15:17:19 +0000

    added a NewWordActivity

---
+++ AndroidRoom/app/src/main/java/android/room/view/ui/NewWordActivity.java
@@ -0,0 +1,35 @@
+package android.room.view.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.room.view.databinding.ActivityNewWordBinding;
+import android.text.TextUtils;
+
+public class NewWordActivity extends AppCompatActivity {
+
+    ActivityNewWordBinding binding;
+    private static final String EXTRA_REPLY = "android.room.view";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityNewWordBinding.inflate(getLayoutInflater());
+        super.onCreate(savedInstanceState);
+        setContentView(binding.getRoot());
+
+        String mEditWordView = binding.editWord.getText().toString();
+
+        binding.buttonSave.setOnClickListener(view -> {
+            Intent replyIntent = new Intent();
+            if (TextUtils.isEmpty(mEditWordView)){
+                setResult(RESULT_CANCELED, replyIntent);
+            } else {
+                replyIntent.putExtra(EXTRA_REPLY, mEditWordView);
+                setResult(RESULT_OK, replyIntent);
+            }
+            finish();
+        });
+    }
+}
