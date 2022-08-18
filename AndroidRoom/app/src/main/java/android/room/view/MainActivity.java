85e962a4694665f3f58968584444ead3f90c053f
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-18 12:34:49 +0000

    a little stuck  with an outdated codelab

--- AndroidRoom/app/src/main/java/android/room/view/MainActivity.java
+++ AndroidRoom/app/src/main/java/android/room/view/MainActivity.java
@@ -1,6 +1,7 @@
 package android.room.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.room.view.databinding.ActivityMainBinding;
@@ -14,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
+
+        final WordListAdapter adapter = new WordListAdapter(new WordListAdapter.WordDiff());
+        binding.recyclerview.setAdapter(adapter);
+        binding.recyclerview.setLayoutManager(new RecyclerView.LayoutManager() {
+            @Override
+            public RecyclerView.LayoutParams generateDefaultLayoutParams() {
+                return null;
+            }
+        });
     }
 }
