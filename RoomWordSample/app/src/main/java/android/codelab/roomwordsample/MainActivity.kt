08f6afc84ac1bafb7332164c6dba4fc91264427c
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-18 19:15:12 +0000

    enabling viewBinding

--- RoomWordSample/app/src/main/java/android/codelab/roomwordsample/MainActivity.kt
+++ RoomWordSample/app/src/main/java/android/codelab/roomwordsample/MainActivity.kt
@@ -1,11 +1,15 @@
 package android.codelab.roomwordsample
 
+import android.codelab.roomwordsample.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
