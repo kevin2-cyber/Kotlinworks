90c9f4ba14d3600cd65aca8347b879ef337f612b
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-18 00:00:03 +0000

    new

--- AndroidRoom/app/src/main/java/android/room/view/MainActivity.java
+++ AndroidRoom/app/src/main/java/android/room/view/MainActivity.java
@@ -3,12 +3,16 @@ package android.room.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.room.view.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
     }
 }
