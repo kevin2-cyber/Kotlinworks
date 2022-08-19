633e1be6ab0ab167873b3383b848dc95c79d2322
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-19 01:40:45 +0000

    initial commit

---
+++ PagingCodelab/app/src/main/java/android/codelab/pagingcodelab/MainActivity.kt
@@ -0,0 +1,11 @@
+package android.codelab.pagingcodelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
