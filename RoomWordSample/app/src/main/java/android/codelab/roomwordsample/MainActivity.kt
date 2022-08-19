432b0723bf217e98573f44ef4c8eff83c2789301
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-19 00:59:56 +0000

    finally solved problem

--- RoomWordSample/app/src/main/java/android/codelab/roomwordsample/MainActivity.kt
+++ RoomWordSample/app/src/main/java/android/codelab/roomwordsample/MainActivity.kt
@@ -1,13 +1,23 @@
 package android.codelab.roomwordsample
 
+import android.app.Activity
 import android.codelab.roomwordsample.databinding.ActivityMainBinding
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val wordViewModel: WordViewModel by viewModels{
+        WordViewModelFactory((application as WordsApplication).repository)
+    }
+    private val newWordActivityRequestCode = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -16,5 +26,29 @@ class MainActivity : AppCompatActivity() {
         val adapter = WordListAdapter()
         binding.recyclerview.adapter = adapter
         binding.recyclerview.layoutManager = LinearLayoutManager(this)
+
+        wordViewModel.allWords.observe(this, Observer { words ->
+            // Update the cached copy of the words in the adapter.
+            words?.let {
+                adapter.submitList(it)
+            }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intentData)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            intentData?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let {
+                reply -> val word = Word(reply)
+                wordViewModel.insert(word)
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                R.string.empty_not_saved,
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
--- RoomWordSample/app/src/main/java/android/codelab/roomwordsample/Word.kt
+++ RoomWordSample/app/src/main/java/android/codelab/roomwordsample/Word.kt
@@ -5,6 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "word_table")
-class Word(
-           @PrimaryKey(autoGenerate = true) val id: Int,
-           @ColumnInfo(name = "word") val word: String)
+data class Word(
+     @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "word")
+    val word: String)
