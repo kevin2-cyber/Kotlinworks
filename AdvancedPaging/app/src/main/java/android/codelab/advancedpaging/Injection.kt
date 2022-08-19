b63851257830397471b6ed46c3541e46699ef6cc
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-19 23:56:41 +0000

    done with injection

--- AdvancedPaging/app/src/main/java/android/codelab/advancedpaging/Injection.kt
+++ AdvancedPaging/app/src/main/java/android/codelab/advancedpaging/Injection.kt
@@ -1,3 +1,31 @@
 package android.codelab.advancedpaging
 
-object Injection {}
+import android.codelab.advancedpaging.api.GithubService
+import android.codelab.advancedpaging.data.GithubRepository
+import androidx.lifecycle.ViewModelProvider
+import androidx.savedstate.SavedStateRegistryOwner
+
+/**
+ * Class that handles object creation.
+ * Like this, objects can be passed as parameters in the constructors and then replaced for
+ * testing, where needed.
+ */
+object Injection {
+
+    /**
+     * Creates an instance of [GithubRepository] based on the [GithubService] and a
+     * [GithubLocalCache]
+     */
+    private fun provideGithubRepository(): GithubRepository {
+        return GithubRepository(GithubService.create())
+    }
+
+    /**
+     * Provides the [ViewModelProvider.Factory] that is then used to get a reference to
+     * [ViewModel] objects.
+     */
+
+    fun provideViewModelFactory(owner: SavedStateRegistryOwner): ViewModelFactory.Factory {
+        return ViewModelFactory(owner, provideGithubRepository())
+    }
+}
