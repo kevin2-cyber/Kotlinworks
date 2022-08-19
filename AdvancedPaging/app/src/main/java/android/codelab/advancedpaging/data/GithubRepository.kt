1a76d448f1f6b5ab9abfd32fbaae0295184b4fed
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-19 23:45:32 +0000

    done with GithubRepository

---
+++ AdvancedPaging/app/src/main/java/android/codelab/advancedpaging/data/GithubRepository.kt
@@ -0,0 +1,86 @@
+package android.codelab.advancedpaging.data
+
+import android.codelab.advancedpaging.api.GithubService
+import android.codelab.advancedpaging.api.IN_QUALIFIER
+import android.codelab.advancedpaging.model.Repo
+import android.codelab.advancedpaging.model.RepoSearchResult
+import android.util.Log
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import retrofit2.HttpException
+import java.io.IOException
+
+private const val GITHUB_STARTING_PAGE_INDEX = 1
+
+class GithubRepository(private val service: GithubService) {
+
+    // keep the list of all results received
+    private val inMemoryCache = mutableListOf<Repo>()
+
+    // shared flow of results, which allows us to broadcast updates so
+    // the subscriber will have the latest data
+    private val searchResults = MutableSharedFlow<RepoSearchResult>(replay = 1)
+
+    // keep the last requested page. When the request is successful, increment the page number.
+    private var lastRequestedPage = GITHUB_STARTING_PAGE_INDEX
+
+    // avoid triggering multiple requests in the same time
+    private var isRequestInProgress = false
+
+    suspend fun getSearchResultStream(query: String): Flow<RepoSearchResult> {
+        Log.d("GithubRepository", "New query: $query")
+        lastRequestedPage = 1
+        inMemoryCache.clear()
+        requestAndSaveData(query)
+
+        return searchResults
+    }
+
+    suspend fun requestMore(query: String) {
+        if (isRequestInProgress) return
+        val successful = requestAndSaveData(query)
+        if (successful) {
+            lastRequestedPage++
+        }
+    }
+
+    suspend fun retry(query: String) {
+        if (isRequestInProgress) return
+        requestAndSaveData(query)
+    }
+
+    private suspend fun requestAndSaveData(query: String): Boolean {
+        isRequestInProgress = true
+        var successful = false
+
+        val apiQuery = query + IN_QUALIFIER
+        try {
+            val response = service.searchRepos(apiQuery, lastRequestedPage, NETWORK_PAGE_SIZE)
+            Log.d("GithubRepository", "response $response")
+            val repos = response.items ?: emptyList()
+            inMemoryCache.addAll(repos)
+            val reposByName = reposByName(query)
+            searchResults.emit(RepoSearchResult.Success(reposByName))
+            successful = true
+        } catch (exception: IOException) {
+            searchResults.emit(RepoSearchResult.Error(exception))
+        } catch (exception: HttpException) {
+            searchResults.emit(RepoSearchResult.Error(exception))
+        }
+        isRequestInProgress = false
+        return successful
+    }
+
+    private fun reposByName(query: String): List<Repo> {
+        // from the in memory cache select only the repos whose name or description matches
+        // the query. Then order the results.
+        return inMemoryCache.filter {
+            it.name.contains(query, true) ||
+                    (it.description != null && it.description.contains(query, true))
+        }.sortedWith(compareByDescending<Repo> { it.stars }.thenBy { it.name })
+    }
+
+    companion object {
+        const val NETWORK_PAGE_SIZE = 30
+    }
+}
