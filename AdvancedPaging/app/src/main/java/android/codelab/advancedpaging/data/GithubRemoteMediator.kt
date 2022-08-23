7a96d2754103b2107041487322dedc37b4eb8c04
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-23 14:36:33 +0000

    Request and save data - implementation

--- AdvancedPaging/app/src/main/java/android/codelab/advancedpaging/data/GithubRemoteMediator.kt
+++ AdvancedPaging/app/src/main/java/android/codelab/advancedpaging/data/GithubRemoteMediator.kt
@@ -1,12 +1,20 @@
 package android.codelab.advancedpaging.data
 
 import android.codelab.advancedpaging.api.GithubService
+import android.codelab.advancedpaging.api.IN_QUALIFIER
+import android.codelab.advancedpaging.db.RemoteKeys
 import android.codelab.advancedpaging.db.RepoDatabase
 import android.codelab.advancedpaging.model.Repo
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
+import okio.IOException
+import retrofit2.HttpException
+
+
+private const val GITHUB_STARTING_PAGE_INDEX = 1
 
 @OptIn(ExperimentalPagingApi::class)
 class GithubRemoteMediator(
@@ -15,7 +23,47 @@ class GithubRemoteMediator(
     private val repoDatabase: RepoDatabase
 ) : RemoteMediator<Int, Repo>() {
     override suspend fun load(loadType: LoadType, state: PagingState<Int, Repo>): MediatorResult {
-        TODO("Not yet implemented")
+        val page = when(loadType) {
+            LoadType.REFRESH -> {
+                TODO()
+            }
+            LoadType.PREPEND -> {
+                TODO()
+            }
+            LoadType.APPEND -> {
+                TODO()
+            }
+        }
+
+        val apiQuery = query + IN_QUALIFIER
+
+        try {
+            val apiResponse = service.searchRepos(apiQuery, page, state.config.pageSize)
+
+            val repos = apiResponse.items
+            val endOfPaginationReached = repos.isEmpty()
+            repoDatabase.withTransaction {
+                // clear all tables in the database
+                if (loadType == LoadType.REFRESH) {
+                    repoDatabase.remoteKeysDao().clearRemoteKeys()
+                    repoDatabase.reposDao().clearRepos()
+                }
+
+                val prevKey = if (page == GITHUB_STARTING_PAGE_INDEX) null else page - 1
+                val nextKey = if (endOfPaginationReached) null else page + 1
+                val keys = repos.map {
+                    RemoteKeys(it.id, prevKey, nextKey)
+                }
+
+                repoDatabase.remoteKeysDao().insertAll(keys)
+                repoDatabase.reposDao().insertAll(repos)
+            }
+            return MediatorResult.Success(endOfPaginationReached)
+        } catch (exception: IOException) {
+            return MediatorResult.Error(exception)
+        } catch (exception: HttpException) {
+            return MediatorResult.Error(exception)
+        }
     }
 
 }
