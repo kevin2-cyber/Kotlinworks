d966799f5421c7f65006b9b2770d99e66b9e8490
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-10-04 12:31:29 +0000

    setting up

--- CameraXApp/app/src/main/java/io/codelab/cameraxapp/MainActivity.kt
+++ CameraXApp/app/src/main/java/io/codelab/cameraxapp/MainActivity.kt
@@ -2,10 +2,15 @@ package io.codelab.cameraxapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.codelab.cameraxapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
