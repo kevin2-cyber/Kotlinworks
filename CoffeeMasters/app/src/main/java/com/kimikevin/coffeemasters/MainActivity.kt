f0c14d055f8e38c003300aa1299a7f2c883e7888
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-05-25 22:27:46 +0000

    change name

--- CoffeeMasters/app/src/main/java/com/kimikevin/coffeemasters/MainActivity.kt
+++ CoffeeMasters/app/src/main/java/com/kimikevin/coffeemasters/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             CoffeeMastersTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Kelvin",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     CoffeeMastersTheme {
-        Greeting("Android")
+        Greeting("Kelvin")
     }
 }
