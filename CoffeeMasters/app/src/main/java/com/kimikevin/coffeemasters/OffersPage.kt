c61015d63cdc5cae02aae55975cdf00edf0e0a75
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-05-27 23:06:00 +0000

    Offers Page

--- CoffeeMasters/app/src/main/java/com/kimikevin/coffeemasters/OffersPage.kt
+++ CoffeeMasters/app/src/main/java/com/kimikevin/coffeemasters/OffersPage.kt
@@ -2,11 +2,14 @@ package com.kimikevin.coffeemasters
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,13 +22,27 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Preview(showBackground = true, widthDp = 400)
+
+
+@Preview(showBackground = true)
 @Composable
-private fun OfferPreview() {
-    Offer(
-        title = "Coffee Masters",
-        description = "Coffee Masters is a coffee shop that sells coffee beans."
-    )
+fun OffersPage() {
+    Column(
+        Modifier.verticalScroll(rememberScrollState())
+    ) {
+        Offer(
+            title = "Early Coffee",
+            description = "10% off. Offer valid from 6am to 9am."
+        )
+        Offer(
+            title = "Welcome Gift",
+            description = "25% off on your first order."
+        )
+        Offer(
+            title = "Welcome Gift",
+            description = "25% off on your first order."
+        )
+    }
 }
 
 @Composable
@@ -33,34 +50,44 @@ fun Offer(
     title: String,
     description: String,
 ) {
-    Image(
-        painter = painterResource(R.drawable.background_pattern),
-        contentDescription = "Background Pattern",
-        contentScale = ContentScale.FillWidth,
-        modifier = Modifier.fillMaxWidth()
-            .height(180.dp)
-    )
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+
+    Box(
         modifier = Modifier.padding(16.dp)
     ) {
-        Text(
-            text = title,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Bold,
+        Image(
+            painter = painterResource(R.drawable.background_pattern),
+            contentDescription = "Background Pattern",
+            contentScale = ContentScale.FillWidth,
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.tertiary)
-                .padding(16.dp)
+                .fillMaxWidth()
+                .height(180.dp)
         )
-        Spacer(
-            modifier = Modifier.height(16.dp)
-        )
-        Text(
-            text = description,
-            style = MaterialTheme.typography.bodyMedium,
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.tertiary)
                 .padding(16.dp)
-        )
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = title,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.tertiary)
+                    .padding(16.dp)
+            )
+            Spacer(
+                modifier = Modifier.height(16.dp)
+            )
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.tertiary)
+                    .padding(16.dp)
+            )
+        }
     }
+
 }
